118d7a7327ac6c0ecbdfd0deec7210d544bf4169
Filip Filipovic <filipfilipovic@Filips-MacBook-Pro.local>
2022-09-04 00:16:41 +0200

    adds snap code

---
+++ snap/packages/snap/src/index.ts
@@ -0,0 +1,61 @@
+import { OnRpcRequestHandler } from '@metamask/snap-types';
+
+/**
+ * Get a message from the origin. For demonstration purposes only.
+ *
+ * @param originString - The origin string.
+ * @returns A message based on the origin.
+ */
+export const getMessage = (originString: string): string =>
+  `Hello, ${originString}!`;
+
+/**
+ * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ *
+ * @param args - The request handler args as object.
+ * @param args.origin - The origin of the request, e.g., the website that
+ * invoked the snap.
+ * @param args.request - A validated JSON-RPC request object.
+ * @returns `null` if the request succeeded.
+ * @throws If the request method is not valid for this snap.
+ * @throws If the `snap_confirm` call failed.
+ */
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+  console.log({
+    where: 'onRpcRequest',
+    origin,
+    request,
+  });
+
+  switch (request.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: `Viking Details!`,
+            description: 'Description',
+            textAreaContent: `
+Contract Details:
+
+Balance in USD: ${request.balance_usd}
+Is verified: ${request.is_verified}
+Name: ${request.name}
+Public URL: ${request.public_url}
+Transaction count: ${request.transaction_count}
+
+Trust Score:
+
+${request.value}% / 100%
+${request.description}
+            `,
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
